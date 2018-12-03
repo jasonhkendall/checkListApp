cba970f6df04bb5df6552c062c58e9841d230d15
jasonhkendall <39975227+jasonhkendall@users.noreply.github.com>
2018-12-02 22:52:34 -0500

    First commit
    Application synchronized with GitHub

---
+++ checkListApp/checkListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checkListApp
+//
+//  Created by Jason Howard Kendall on 12/2/18.
+//  Copyright © 2018 Jason Howard Kendall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
